a13dabe5cb06f6985c72453867c5adda24854a7b
bosscheng <25022549@qq.com>
2014-12-11 16:14:42 +0800

    update method
    update method

--- util-core.js
+++ util-core.js
@@ -10,8 +10,12 @@
 * 在原型链上添加方法 method 用于创建方法的
 * */
 Function.prototype.method = function (name, func) {
-    // 很好的利用了this对象
-    this.prototype[name] = func;
+    // 符合条件的时候才会增加
+    if(!this.prototype[name]){
+        // 很好的利用了this对象
+        this.prototype[name] = func;    
+    }
+    
     return this;
 }
 
