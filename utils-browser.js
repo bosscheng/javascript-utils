b64359afb738c3a74d12ca2ce52349da0dc902bd
bosscheng <25022549@qq.com>
2014-12-26 09:55:50 +0800

    add method
    add method

--- utils-browser.js
+++ utils-browser.js
@@ -1,9 +1,25 @@
 /**
- * 二蛋，记得添加备注啊
+ * browser 上面的方法
  * Author: Administrator
  * Data: 14-12-18
  */
 
-define(function (require, exports, module) {
-    //..
-});
+var org = org ||{};
+
+org.utils = org.utils || {}
+
+;(function(util){
+    
+    // 获取当时时间 格式为 xxxx-yy-hh dd:ss:ee
+    util.getNowTime = function(){
+        var time = new Date();
+        var year = time.getFullYear();
+        var month = time.getMonth + 1;
+        var day = time.getDate();
+        var hour = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        return year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconeds;  
+    }
+    
+})(org.utils)
