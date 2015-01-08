ab6d9a17b039d3ba4c7f494fdc68c639e6f7a69d
bosscheng <25022549@qq.com>
2015-01-08 19:29:58 +0800

    add method
    Add methods

--- utils-node.js
+++ utils-node.js
@@ -4,6 +4,55 @@
  * Data: 14-12-18
  */
 
-define(function (require, exports, module) {
-    //..
-});
+org.utils = org.utils || {};
+
+;(function(utils){
+    
+    utils.setTimeout = function(funktion,delay){
+        return window.setTimeout(function(){
+            try{
+                funktion();
+            }
+            catch(x){
+                // 报错！
+            }
+            
+        },delay);
+    }
+    
+    
+    
+    var getNowTime = function(){
+        var time = new Date();
+        var year = time.getFullYear();
+        var month = time.getMonth() + 1;
+        var day = time.getDate();
+        var hour = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        return year + '-' + month + '-' + day + " " + hour + ":" + minutes + ":" + seconds;
+    }
+    
+    var _log = function(){
+        _showLog("log",arguments);
+    }
+    
+    var _showLog = function(level,args){
+        if(window.console){
+            var logger = window.console[level];
+            iftypeof logger === "Function")｛
+                logger.apply(window.console,args);
+            ｝
+            
+        }
+    }
+    
+    utils.log = function(){
+        var currentTime = "[" + getNowTime() + "]";
+        _log(currentTime,arguments);
+    }
+    
+    
+    
+    
+})(org.utils);
