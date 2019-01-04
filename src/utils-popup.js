1816905bfe7a29b3e72c00b53c348d6763a2ba06
bosscheng <25022549@qq.com>
2019-01-04 17:22:56 +0800

    add methods
    add methods

--- src/utils-popup.js
+++ src/utils-popup.js
@@ -15,5 +15,66 @@ var instances = {};
 // popup 管理对象。
 var PopupManager = {
 
+    modalFade: true,
+    modalStack: [],  // modal stack 堆栈信息。
+
+    // 获取实例对象
+    getInstance: function (id) {
+
+    },
+
+    register: function (id, instance) {
+
+    },
+
+    deregister: function (id) {
+
+    },
+
+    nextZIndex: function () {
+
+    },
+
+    doOnModalClick: function () {
+
+    },
+
+    openModal: function () {
+
+    },
+
+    closeModal: function () {
+
+    }
 
 };
+
+
+Object.defineProperty(PopupManager, 'zIndex', {
+    configurable: true,
+    get() {
+        if (!hasInitZIndex) {
+            //zIndex =
+            // 初始化的时候设置的zIndex 大小。
+            hasInitZIndex = true;
+        }
+        return zIndex;
+    },
+    set(value) {
+        zIndex = value;
+    }
+});
+
+// 获取到最顶层的 popup
+var getTopPopup = function () {
+
+};
+
+window.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+
+        //    获取到顶层的 popup，然后关闭掉。
+    }
+});
+
+
