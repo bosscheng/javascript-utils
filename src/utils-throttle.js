d7ee810d6958ea13636aa552aaf84fa4a6ba47ba
bosscheng <25022549@qq.com>
2019-04-09 14:22:59 +0800

    Create utils-throttle.js

---
+++ src/utils-throttle.js
@@ -0,0 +1,23 @@
+/**
+ * 节流
+ */
+
+
+/**
+ *
+ * @param fn 函数
+ * @param wait 间隔时间
+ * @returns {Function}
+ */
+function throttle(fn, wait) {
+    var pre = new Date();
+    return function () {
+        const args = arguments;
+        var now = new Date();
+
+        if (now - pre > wait) {
+            fn.apply(this, args);
+            pre = new Date();
+        }
+    }
+}
