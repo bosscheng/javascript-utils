9033638651dc1661ea6ae85c0a8620b84e3d149e
万成 <25022549@qq.com>
2019-04-14 12:53:48 +0800

    add methods

--- src/utils-is.js
+++ src/utils-is.js
@@ -422,7 +422,60 @@ var isDigit = function (value) {
     } else {
         return true
     }
-}
+};
+
+/**
+ *
+ * @param url
+ * @returns {boolean}
+ */
+var isAbsoluteURL = function (url) {
+    return /^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(url);
+};
+
+/**
+ *
+ * @type {function(*=): boolean}
+ */
+var isUrlSameOrigin = function () {
+    var msie = /(msie|trident)/i.test(navigator.userAgent);
+    var urlParsingNode = document.createElement('a');
+    var originUrl;
+
+    function resolveUrl(url) {
+        var href = url;
+
+        if (msie) {
+            urlParsingNode.setAttribute('href', href);
+            href = urlParsingNode.href;
+        }
+
+        urlParsingNode.setAttribute('href', href);
+
+
+        return {
+            href: urlParsingNode.href,
+            protocol: urlParsingNode.protocol ? urlParsingNode.protocol.replace(/:$/, '') : '',
+            host: urlParsingNode.host,
+            search: urlParsingNode.search ? urlParsingNode.search.replace(/^\?/, '') : '',
+            hash: urlParsingNode.hash ? urlParsingNode.hash.replace(/^#/, '') : '',
+            hostname: urlParsingNode.hostname,
+            port: urlParsingNode.port,
+            pathname: (urlParsingNode.pathname.charAt(0) === '/') ?
+                urlParsingNode.pathname :
+                '/' + urlParsingNode.pathname
+        }
+    }
+
+    originUrl = resolveUrl(window.location.href);
+
+
+    return function (requestUrl) {
+        var paresd = (isString(requestUrl) ? resolveUrl(requestUrl) : requestUrl);
+        return paresd.protocol === originUrl.protocol && paresd.host === originUrl.host;
+    }
+
+}();
 
 /*
  * 判断属性值 a 和 b 是否相等，
