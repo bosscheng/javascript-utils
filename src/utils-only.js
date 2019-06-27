430ed05e650b4c247914f68e435faced35152422
万成 <25022549@qq.com>
2019-06-27 18:30:03 +0800

    Create utils-only.js

---
+++ src/utils-only.js
@@ -0,0 +1,14 @@
+/*
+* date: 2019-06-27
+* desc:
+*/
+function only(obj, keys) {
+    obj = obj || {};
+    // 这种写法 ，吊炸天 无限空格。
+    if ('string' == typeof keys) keys = keys.split(/ +/);
+    return keys.reduce(function (ret, key) {
+        if (null == obj[key]) return ret;
+        ret[key] = obj[key];
+        return ret;
+    }, {});
+}
