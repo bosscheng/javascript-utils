1f1727839f5160711f0810c5434fc9ea7948a1c5
万成 <25022549@qq.com>
2019-08-25 22:54:50 +0800

    add methods

---
+++ src/array/utils-getEmptyList.js
@@ -0,0 +1,11 @@
+/*
+* date: 2019-08-25
+* desc:
+*/
+const getEmptyList = (len = 100) => {
+    let xAxisArray = [];
+    for (let i = 0; i < len; i++) {
+        xAxisArray.push('')
+    }
+    return xAxisArray
+};
---
+++ src/date/utils-getMinuteRange.js
@@ -0,0 +1,68 @@
+/*
+* date: 2019-08-25
+* desc:
+*/
+const getMinuteRange = (startTime, endTime, step) => {
+    let result = [];
+
+    const _compareTime = function (time1, time2) {
+        const value1 = _parseTime(time1);
+        const value2 = _parseTime(time2);
+
+        const minutes1 = value1.minutes + value1.hours * 60;
+        const minutes2 = value2.minutes + value2.hours * 60;
+
+        if (minutes1 === minutes2) {
+            return 0;
+        }
+
+        return minutes1 > minutes2 ? 1 : -1;
+    };
+
+    const _parseTime = function (time) {
+        const values = (time || '').split(':');
+        if (values.length >= 2) {
+            const hours = parseInt(values[0], 10);
+            const minutes = parseInt(values[1], 10);
+
+            return {
+                hours,
+                minutes
+            };
+        }
+        /* istanbul ignore next */
+        return null;
+    };
+
+    const _formatTime = function (time) {
+        return (time.hours < 10 ? '0' + time.hours : time.hours) + ':' + (time.minutes < 10 ? '0' + time.minutes : time.minutes);
+    };
+
+    const _nextTime = function (time, step) {
+        const timeValue = _parseTime(time);
+        const stepValue = _parseTime(step);
+
+        const next = {
+            hours: timeValue.hours,
+            minutes: timeValue.minutes
+        };
+
+        next.minutes += stepValue.minutes;
+        next.hours += stepValue.hours;
+
+        next.hours += Math.floor(next.minutes / 60);
+        next.minutes = next.minutes % 60;
+
+        return _formatTime(next);
+    };
+
+    if (startTime && endTime && step) {
+        let current = startTime;
+        while (_compareTime(current, endTime) <= 0) {
+            result.push(current);
+            current = _nextTime(current, step);
+        }
+    }
+
+    return result;
+};
