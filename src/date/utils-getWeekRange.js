dbe3ec2639c00210558f49471a116e641a07b697
万成 <25022549@qq.com>
2019-09-06 16:59:08 +0800

    add files

---
+++ src/date/utils-getWeekRange.js
@@ -0,0 +1,18 @@
+/*
+* date: 2019-09-06
+* desc:
+*/
+function getWeekRange(year, week) {
+    let d = new Date(year, 0, 1);
+    let time = d.getTime();
+    let t = week * 7 * 24 * 3600 * 1000;
+    time += t;
+    d = new Date(time);
+    let w = d.getDay();
+    let mon = new Date(time - (w - 1) * 24 * 3600 * 1000);
+    let sun = new Date(time + ((7 - w) * 24 * 3600 * 1000));
+    return {
+        mon,
+        sun
+    }
+}
