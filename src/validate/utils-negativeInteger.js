247cb19fe173ef4a593d28ae9c4c84593706969c
bosscheng1210 <25022549@qq.com>
2021-01-05 11:12:59 +0800

    add files

---
+++ src/validate/utils-negativeInteger.js
@@ -0,0 +1,9 @@
+/**
+ * Date:2021/1/5
+ * Desc:
+ */
+
+// 负整数
+function negative_integer() {
+    return '^-[1-9]+\\d*$';
+}
---
+++ src/validate/utils-positiveInteger.js
@@ -0,0 +1,10 @@
+/**
+ * Date:2021/1/5
+ * Desc: 校验使用到的正则表达式。
+ */
+
+// 正整数
+function positive_integer() {
+    return '^[1-9]+\\d*$';
+}
+
