c69463d8422b7291fc7a093a968cb99b6736029d
bosscheng1210 <25022549@qq.com>
2021-01-11 10:55:59 +0800

    Create utils-formatFileSize.js

---
+++ src/number/utils-formatFileSize.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param value  单位 Bytes
+ * @returns {string} 
+ */
+function formatFileSize(value) {
+    if (null == value || value == '') {
+        return "0 Bytes";
+    }
+    var unitArr = new Array("Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+    var index = 0;
+    var srcsize = parseFloat(value);
+    index = Math.floor(Math.log(srcsize) / Math.log(1024));
+    var size = srcsize / Math.pow(1024, index);
+    size = size.toFixed(2);//保留的小数位数
+    return size + unitArr[index];
+}
