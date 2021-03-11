b4fdccea12452b50341c038d0b526e58a8bd8340
wancheng1990 <25022549@qq.com>
2021-03-11 15:40:41 +0800

    Create utils-saveBlobToFile.js

---
+++ src/blob/utils-saveBlobToFile.js
@@ -0,0 +1,16 @@
+/**
+ * save blob
+ */
+function saveBlob() {
+    (function () {
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.style.display = 'none';
+        return function saveData(blob, fileName) {
+            const url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.click();
+        };
+    }());
+}
