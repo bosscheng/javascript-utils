3536509e00440234182af4c5500e156c6fd0befe
wancheng1990 <25022549@qq.com>
2021-03-24 11:48:58 +0800

    Update utils-getDeviceList.js

--- src/media/utils-getDeviceList.js
+++ src/media/utils-getDeviceList.js
@@ -3,7 +3,7 @@
  * let ePromise = navigator.mediaDevices.enumerateDevices
  *
  * 获取到音频设备的属性
- * deviceId 设备属性
+ * deviceId 设备属性  default 默认的设备（只有一个）,communications 通讯中的设备（只有一个) id 设备的id 会和前面的默认设备重复
  * label    设备名称
  * kind     设备种类
  * groupId  两个设备groupId相同, 说明是同一个物理设备
@@ -15,9 +15,9 @@ function getDeviceList() {
             reject('enumerateDevices is not supported');
         } else {
             let deviceList = {
-                audioinput: [],
-                videoinput: [],
-                audiooutput: []
+                audioinput: [], // 音频输入 (麦克风)
+                videoinput: [], // 视频输入 (摄像头)
+                audiooutput: [] // 音频输出 (扬声器)
             };
             navigator.mediaDevices
                 .enumerateDevices()
