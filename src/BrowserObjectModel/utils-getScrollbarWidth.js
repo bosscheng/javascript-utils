b620a5c59257bb4a78cd712885429b41e5a75d0d
wancheng1990 <25022549@qq.com>
2021-03-31 14:46:59 +0800

    add getscrollbarwidth

---
+++ src/BrowserObjectModel/utils-getScrollbarWidth.js
@@ -0,0 +1,14 @@
+function getScrollbarWidth() {
+    const div = document.createElement('div');
+    div.style.visibility = 'hidden';
+    div.style.width = '100px';
+    document.body.appendChild(div);
+    const offsetWidth = div.offsetWidth;
+    div.style.overflow = 'scroll';
+    const childDiv = document.createElement('div');
+    childDiv.style.width = '100%';
+    div.appendChild(childDiv);
+    const childOffsetWidth = childDiv.offsetWidth;
+    div.parentNode.removeChild(div);
+    return offsetWidth - childOffsetWidth;
+}
