1a545097ca32bf5e15d775b6afd7e6eee06ed09a
wancheng1990 <25022549@qq.com>
2021-04-19 16:55:32 +0800

    add methods

--- src/validate/utils-loginName.js
+++ src/validate/utils-loginName.js
@@ -7,6 +7,11 @@ function loginName(str) {
     return /^[a-zA-Z0-9-]+$/.test(str)
 }
 
+/**
+ * @desc 强制必须得有(a-zA-Z)一次，其他的6到20位，必须得是 a-zA-Z0-9_- 其中。
+ * @param str
+ * @returns {boolean}
+ */
 function loginName2(str) {
     return /^(?=.*[a-zA-Z])[a-zA-Z0-9_-]{6,20}$/.test(str);
 }
