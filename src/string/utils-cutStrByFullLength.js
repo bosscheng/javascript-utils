77a6f66c0e1a012ace3c668691fd8b75466caf7c
wancheng1990 <25022549@qq.com>
2021-06-08 14:09:20 +0800

    Update utils-cutStrByFullLength.js

--- src/string/utils-cutStrByFullLength.js
+++ src/string/utils-cutStrByFullLength.js
@@ -2,9 +2,9 @@
  * Date:2020/10/28
  * Desc:
  */
-function cutStrByFullLength(str = '', maxLength) {
+function cutStrByFullLength(str = '', maxLength, suffix) {
     let showLength = 0
-    return str.split('').reduce((pre, cur) => {
+    let result = str.split('').reduce((pre, cur) => {
         const charCode = cur.charCodeAt(0)
         if (charCode >= 0 && charCode <= 128) {
             showLength += 1
@@ -16,4 +16,9 @@ function cutStrByFullLength(str = '', maxLength) {
         }
         return pre
     }, '')
+    if (suffix) {
+        result += suffix;
+    }
+
+    return result;
 }
