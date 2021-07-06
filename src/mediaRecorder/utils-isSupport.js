e3c1f9affd06ac842a68280cd5d12e44f221a6f8
wancheng1990 <25022549@qq.com>
2021-07-06 13:46:20 +0800

    Update utils-isSupport.js

--- src/mediaRecorder/utils-isSupport.js
+++ src/mediaRecorder/utils-isSupport.js
@@ -2,13 +2,17 @@ function utilsIsSupport(){
     var types = ["video/webm",
         "audio/webm",
         "video/webm",
-        "video/webm\;codecs=vp9",
-        "video/webm\;codecs=vp8",
-        "video/webm\;codecs=daala",
-        "video/webm\;codecs=h264",
-        "audio/webm\;codecs=opus",
+        "video/webm;codecs=vp9",
+        "video/webm;codecs=vp8",
+        "video/webm;codecs=daala",
+        "video/webm;codecs=h264",
+        "audio/webm;codecs=opus",
         'video/mp4\;codecs="avc1.424028, mp4a.40.2"',
         'video/mp4',
+        'video/webm;codecs=vp9,opus',
+        'video/webm;codecs=vp8,opus',
+        'video/webm;codecs=h264,opus',
+        'video/mp4;codecs=h264,aac',
         "video/mpeg"];
 
     for (var i in types) {
