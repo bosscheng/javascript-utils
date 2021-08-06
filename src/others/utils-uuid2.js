9f6265472874cb0d3bf63a2a72abfb3d5595c186
bosscheng <25022549@qq.com>
2021-08-06 16:01:46 +0800

    update files

--- src/others/utils-uuid2.js
+++ src/others/utils-uuid2.js
@@ -1,10 +1,14 @@
 /**
  * Date:2021/1/14
- * Desc: 13位的uuid
+ * Desc: 16位的uuid
  */
 function uuid() {
-    return 'xxxxxxxx-4xxx'.replace(/[xy]/g, function (c) {
+    return 'xxxxxxxxxxxx4xxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
         return v.toString(16)
     })
 }
+
+
+const result = uuid();
+console.log(result,result.length)
--- src/others/utils-uuid3.js
+++ src/others/utils-uuid3.js
@@ -14,3 +14,5 @@ function uuid() {
         lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
         lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
 }
+
+console.log(uuid());
