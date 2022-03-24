0092ecdae1abe573bc8e470597ab934689f3510f
bosscheng <25022549@qq.com>
2022-03-24 22:24:31 +0800

    Create utils-stringToArrayBuffer.js

---
+++ src/arrayBuffer/utils-stringToArrayBuffer.js
@@ -0,0 +1,9 @@
+function utilsStringToArrayBuffer(string){
+    const view = new Uint8Array(new ArrayBuffer(string.length));
+    //
+    for (let i = 0; i < string.length; i++) {
+        view[i] = string.charCodeAt(i);
+    }
+
+    return view.buffer;
+}
