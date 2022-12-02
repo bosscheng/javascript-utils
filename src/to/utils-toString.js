919da6a3ca1b13e336507a6915e16e3fadca918d
bosscheng <25022549@qq.com>
2022-12-02 10:01:06 +0800

    Update utils-toString.js

--- src/to/utils-toString.js
+++ src/to/utils-toString.js
@@ -15,3 +15,35 @@ function toString2(value) {
             ? JSON.stringify(value, null, 2)
             : String(value)
 }
+
+
+function toString3(value) {
+    const nativeToString = Object.prototype.toString;
+
+    function isErrorLike(error) {
+        switch (nativeToString.call(error)) {
+            case '[object Error]':
+                return true;
+            case '[object Exception]':
+                return true;
+            case '[object DOMException]':
+                return true;
+            default:
+                try {
+                    return error instanceof Error;
+                } catch (e) {
+                    return false;
+                }
+        }
+    }
+
+    if (isErrorLike(value)) {
+        return value.toString();
+    }
+
+    return value == null
+        ? '' :
+        typeof value === 'object'
+            ? JSON.stringify(value, null, 2)
+            : String(value)
+}
